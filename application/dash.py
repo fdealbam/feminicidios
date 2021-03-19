4efe0e6e3d70aa33ae8e9e110848c58571be1f93
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-03-19 13:09:01 -0600

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -1,3 +1,4 @@
+
 import dash
 import matplotlib.pyplot as plt 
 import dash_bootstrap_components as dbc
@@ -229,9 +230,7 @@ fem_filter1.fillna(0, inplace=True)
 fem_filter1['Total']=fem_filter1['Total'].astype(int)
 
 
-
-
-############################################### filtro para estados(3) e identifiación de mpios y abs feminicidios
+############################################### filtro para estados(4) e identifiación de mpios y abs feminicidios
 
 # estado 1
 estado1=fem_filter1[fem_filter1.Entidad=='México']
@@ -252,6 +251,7 @@ edo1mpio4v=edo1orden.iloc[3]['Total']
 edo1mpio5=edo1orden.iloc[4]['Municipio']
 edo1mpio5v=edo1orden.iloc[4]['Total']
 
+
 # estado 2
 estado2=fem_filter1[fem_filter1.Entidad=='Veracruz de Ignacio de la Llave']
 edo2orden=estado2[['Municipio','Total']].sort_values('Total',ascending=False)
@@ -271,6 +271,7 @@ edo2mpio4v=edo2orden.iloc[3]['Total']
 edo2mpio5=edo2orden.iloc[4]['Municipio']
 edo2mpio5v=edo2orden.iloc[4]['Total']
 
+
 # estado 3
 estado3=fem_filter1[fem_filter1.Entidad=='Ciudad de México']
 edo3orden=estado3[['Municipio','Total']].sort_values('Total',ascending=False)
@@ -289,6 +290,7 @@ edo3mpio4v=edo3orden.iloc[3]['Total']
 #5
 edo3mpio5=edo3orden.iloc[4]['Municipio']
 edo3mpio5v=edo3orden.iloc[4]['Total']
+
 
 # estado 4
 estado4=fem_filter1[fem_filter1.Entidad=='Jalisco']
@@ -312,7 +314,7 @@ edo4mpio5v=edo4orden.iloc[4]['Total']
 
 
 ######################################################### tablas Ranking municipios
-############## tabla 1
+# tabla 1
 patabla1 = {'Mpio' : [edo1mpio1,edo1mpio2,edo1mpio3,edo1mpio4,edo1mpio5],
             'Casos': [edo1mpio1v,edo1mpio2v,edo1mpio3v,edo1mpio4v,edo1mpio5v],}
 
@@ -338,9 +340,53 @@ patabla4 = {'Mpio'  : [edo4mpio1,edo4mpio2,edo4mpio3,edo4mpio4,edo4mpio5],
             'Casos' : [edo4mpio1v,edo4mpio2v,edo4mpio3v,edo4mpio4v,edo4mpio5v],}
 
 patabla4a = pd.DataFrame (patabla4, columns = ['Mpio','Casos'])
+
+
+
+
+######################################################### Graf. Tasas de feminicidios por entidad 2015-2020
+
+junto1 = pd.read_csv('https://raw.githubusercontent.com/fdealbam/feminicidios/main/application/POB_15_20.csv')
+fem15_20 = femi15_20[['Entidad', 'Total2015', 'Total2016', 'Total2017',
+       'Total2018', 'Total2019', 'Total2020']]
+
+junto15_20 = fem15_20.merge(junto1,right_on='NOM_ENT',left_on='Entidad')
+junto15_20['Totfem1520']=junto15_20[['Total2015', 'Total2016', 'Total2017', 'Total2018','Total2019', 'Total2020']].sum(1)
+junto15_20['Totpob1520']=junto15_20[['POB15', 'POB16', 'POB17', 'POB18','POB19', 'POB20']].sum(1)
+junto15_20['Tasa1520']=((junto15_20.Totfem1520/junto15_20.Totpob1520)*100000).round(2)
+
+TasasFem15_20index=junto15_20[['Entidad','Totfem1520','Totpob1520','Tasa1520']].sort_values('Tasa1520',ascending=False)
+
+
+######################################################### Grafica Tasas
+
+graf_tasafem = go.Figure()
+graf_tasafem.add_trace(go.Bar(x=TasasFem15_20index['Entidad'],y=TasasFem15_20index['Tasa1520'],
+                marker_color='sandybrown'  # cambiar nuemeritos de rgb
+                ))
+
+graf_tasafem.update_layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis_tickangle=-45,
+    template = 'simple_white',
+    #title='Tasa feminicidio periodo 2015-2020',
+    xaxis_tickfont_size= 12,
+    yaxis=dict(
+        title='Tasa cada 100 000 habitantes',
+        titlefont_size=14,
+        tickfont_size=12,
+        titlefont_family= "Monserrat"),
+    autosize=True,
+    width=2100,
+    height=600
+    )
+
+
+
 ######################################################### MAPAS 3estados con más feminicidios
 
-#concat2 = concat[concat.Entidad == "Veracruz de Ignacio de la Llave"]
+#concat2 = fem_filter1[fem_filter1.Entidad == "Veracruz de Ignacio de la Llave"]
 #concat.plot("NOM_ENT", cmap= "Oranges", legend=True, k=5)
 #plt.axis("off")
 #plt.savefig("ver.png", dpi= 120)
@@ -402,8 +448,8 @@ body = html.Div([
     
      dbc.Row(
            [
-               dbc.Col(html.H1("Casos Anuales"),
-                        width={'size': 5,  "offset":1 }),
+               dbc.Col(html.H1("Casos Anuales (valores absolutos)"),
+                        width={'size': 8,  "offset":1 }),
             ]),
 
        html.Hr(),
@@ -451,8 +497,8 @@ body = html.Div([
 #---------Grafica mensual
      dbc.Row(
            [
-               dbc.Col(html.H1("Casos mensuales (2015-2020)"),
-                        width={'size': 6,  "offset":1 }),
+               dbc.Col(html.H1("Casos mensuales (2015-2020) (Valores absolutos)"),
+                        width={'size': 8,  "offset":1 }),
             ]),
    
     dbc.Row(
@@ -472,8 +518,8 @@ body = html.Div([
     #títulos
      dbc.Row(
            [
-               dbc.Col(html.H1("Municipios en entidades con más casos"),
-                        width={'size': 7,  "offset":1 }),
+               dbc.Col(html.H1("Municipios en entidades con más casos (valores absolutos)"),
+                        width={'size': 10,  "offset":1 }),
             ]),
 
        html.Hr(),
@@ -489,7 +535,7 @@ body = html.Div([
            ], justify= "center"),
     dbc.Row([
                dbc.Col(dbc.CardImg(src="https://github.com/Aeelen-Miranda/feminicidios/blob/main/application/static/mx.jpeg?raw=true")),
-               dbc.Col(dbc.CardImg(src="https://github.com/Aeelen-Miranda/feminicidios/blob/main/application/static/cdmx.jpeg?raw=true")),
+               dbc.Col(dbc.CardImg(src="https://github.com/Aeelen-Miranda/feminicidios/blob/main/application/static/ver.jpeg?raw=true")),
            ]),
        html.Hr(),
 
@@ -522,10 +568,10 @@ body = html.Div([
                       
                dbc.Col(html.H3("Jalisco"),
                         width=4, lg={'size': 1,  "offset": 4, }),
-           ]),
+           ], justify= "center"),
     
     dbc.Row([
-               dbc.Col(dbc.CardImg(src="https://github.com/Aeelen-Miranda/feminicidios/blob/main/application/static/ver.jpeg?raw=true")),
+               dbc.Col(dbc.CardImg(src="https://github.com/Aeelen-Miranda/feminicidios/blob/main/application/static/cdmx.jpeg?raw=true")),
                dbc.Col(dbc.CardImg(src="https://github.com/Aeelen-Miranda/feminicidios/blob/main/application/static/jal.jpeg?raw=true")),
            ]),
    
@@ -548,6 +594,21 @@ body = html.Div([
        html.Hr(),
        html.Hr(),
        html.Hr(),
+     html.Hr(),
+       
+#---------Grafica mensual
+     dbc.Row(
+           [
+               dbc.Col(html.H1("Tasa de feminicidios, según entidad (2015-2020)"),
+                        width={'size': 8,  "offset":1 }),
+            ]),
+   
+    dbc.Row(
+        [
+            dbc.Col(dcc.Graph(figure=graf_tasafem, config= "autosize")),
+        ]),
+
+       html.Hr(),
     
 
 # nuevo
@@ -567,14 +628,23 @@ body = html.Div([
                     "Existe una mayor atención institucional y atención publica al fenómeno en los últimos "
                     "años, lo que hace que todos seamos más vigilantes al respecto. No obstante, sin duda aún "
                     "hace falta más acción social y más intervención institucional para "
-                    "establecer estrategias efectivas de prevención y denuncia. "
-                    , className="lead"),
+                    "establecer estrategias efectivas de prevención y denuncia. ",
+                    className="lead"),
                 html.P(
                     "Este es un ejercicio institucional de informar a la sociedad, cuya fuente de información "
                     "es el Secretariado Ejecutivo Ejecutivo Nacional del Sistema Nacional de Seguridad Nacional "
                     "(SENSNSP, Gobierno Federal), que seguramente puede ser completado con otras fuentes de "
                     "información gubernamentales y de información proveniente de organizaciones civiles que "
-                    "dan seguimiento al tema. ",className="lead"),
+                    "dan seguimiento al tema. En ningún caso, la información aquí presentada representa algún "
+                    "posicionamiento partidista, personal o institucional ni representa opinión o postura alguna "
+                    "sobre el fenómeno.  ",
+                    className="lead"),
+                html.H6("Metodología "),
+                html.P(
+                    "Esta información fue tratada con el lenguaje de programación Python y varias de las librerías "
+                    "que automatizan la recurrencia a la fuente de información, lo que hace interactivo este Dashboard.",
+                    className="lead"),
+                    
             ], fluid=True,
         )
     ],
