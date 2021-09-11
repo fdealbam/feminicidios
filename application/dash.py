75542a2a3e506435306d9181c8b4bd7433d9e67e
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-09-10 22:16:03 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -549,7 +549,7 @@ femi15_21 = ff[[
  'Enero20','Febrero20','Marzo20','Abril20','Mayo20','Junio20','Julio20',
  'Agosto20','Septiembre20','Octubre20','Noviembre20','Diciembre20',
     
- 'Enero21','Febrero21','Marzo21','Abril21','Mayo21','Junio21',#'Julio21',
+ 'Enero21','Febrero21','Marzo21','Abril21','Mayo21','Junio21','Julio21',
 # 'Agosto21','Septiembre21','Octubre21','Noviembre21','Diciembre21'
              ]]
 
@@ -576,7 +576,7 @@ femi15_21['Total2020']= femi15_21[[ 'Enero20', 'Febrero20', 'Marzo20', 'Abril20'
                                'Noviembre20', 'Diciembre20',]].sum(axis=1)
 
 femi15_21['Total2021']= femi15_21[[ 'Enero21','Febrero21', 'Marzo21', 'Abril21','Mayo21',
-                                   'Junio21',#'Julio21','Agosto21','Septiembre21','Octubre21',
+                                   'Junio21','Julio21',#'Agosto21','Septiembre21','Octubre21',
                                    #'Noviembre21','Diciembre21'
                                   ]].sum(axis=1)
 
@@ -611,7 +611,7 @@ pagra = ff[[
  'Enero20', 'Febrero20', 'Marzo20', 'Abril20', 'Mayo20', 'Junio20', 'Julio20', 'Agosto20',
     'Septiembre20','Octubre20', 'Noviembre20', 'Diciembre20',
 
- 'Enero21', 'Febrero21', 'Marzo21', 'Abril21', 'Mayo21', 'Junio21',# 'Julio21',# 'Agosto21',
+ 'Enero21', 'Febrero21', 'Marzo21', 'Abril21', 'Mayo21', 'Junio21', 'Julio21',# 'Agosto21',
   #  'Septiembre21','Octubre21','Noviembre21','Diciembre21'
             ]]
 
@@ -1075,7 +1075,7 @@ body = html.Div([
                                        width={'size': 11,  "offset":1 })]),
        dbc.Row(
            [        
-               dbc.Col(html.H5("(hasta junio 2021)"),
+               dbc.Col(html.H5("(hasta julio 2021)"),
                                        width={ 'size': 3, "offset":1 }),
 
             ]),
